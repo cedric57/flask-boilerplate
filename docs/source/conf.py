e4905f12d434820aceafbc2d9867fdf8a2fed63c
Cedric Grun <cedric.grun@gmail.com>
2025-01-15 10:16:55 +0100

    update for mypy

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,10 +13,10 @@ author = 'Cedric Grun'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = []     # type: List[str]
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = []   # type: List[str]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output

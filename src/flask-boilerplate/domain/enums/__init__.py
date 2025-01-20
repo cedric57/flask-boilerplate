95768e4be1ba61c00ee58a8e73929f96a7785708
Cedric <cedric.grun@gmail.com>
2025-01-20 17:47:05 +0100

    Create domain enums __init__.py

---
+++ src/flask-boilerplate/domain/enums/__init__.py
@@ -0,0 +1,9 @@
+"""
+This package contains enumeration classes used in the domain layer.
+
+Enumerations are used to define a set of named values, which can be used to represent
+a fixed set of constants in a more readable and maintainable way.
+"""
+
+# Example enumeration import
+#from .example_enum import ExampleEnum

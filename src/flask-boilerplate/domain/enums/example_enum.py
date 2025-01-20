7f7414be702377dbac9b3717eec87c0f2393083e
Cedric <cedric.grun@gmail.com>
2025-01-20 17:50:38 +0100

    Create example_enum.py

---
+++ src/flask-boilerplate/domain/enums/example_enum.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+class ExampleEnum(Enum):
+    """
+    ExampleEnum is an enumeration that represents a set of named constants.
+    
+    Attributes:
+        OPTION_ONE (str): Description for option one.
+        OPTION_TWO (str): Description for option two.
+        OPTION_THREE (str): Description for option three.
+    """
+    OPTION_ONE = "Option 1"
+    OPTION_TWO = "Option 2"
+    OPTION_THREE = "Option 3"
+  
